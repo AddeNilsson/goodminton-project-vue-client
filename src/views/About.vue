a9dfc7bce1248fad4562e6a8dd9850e181e6332a
Adde Nilsson <adde.a.nilsson@gmail.com>
2020-01-28 10:17:51 +0100

    added about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home flex flex-center">
+    <div class="col-4">
+      <card>
+        <card-content>
+          <h1>About</h1>
+          <p class="text-body">
+            This project's purpose is to be viewed upon as a badminton
+            playing frontend developers way to get into Vue.js.
+          </p>
+          <p class="text-body">
+            Another version, written in React, is to be found
+            <a href="https://goodminton-tracker.firebaseapp.com/" target="_blank">
+              Here
+            </a>
+          </p>
+          <p class="text-body">
+            Feel free to create an account and have a look around!
+          </p>
+        </card-content>
+      </card>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import { Card, CardContent } from '../components/Card';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    Card, CardContent,
   },
 };
 </script>
